464ed8b5e297a415a4ab779c33295c95db059ed8
Tih Samuel <samuel.tih.22@gmail.com>
2021-08-05 10:20:24 +0100

    Component created

---
+++ lib/widgets/catalog_page_form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class CatalogPageForm extends StatefulWidget {
+  @override
+  _CatalogPageFormState createState() => _CatalogPageFormState();
+}
+
+class _CatalogPageFormState extends State<CatalogPageForm> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
